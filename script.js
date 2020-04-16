651f5e227af9a6d84cfb6f169a7e380bd80e742b
mlevi17 <62549834+mlevi17@users.noreply.github.com>
2020-04-16 23:12:49 +0200

    1.1

---
+++ script.js
@@ -0,0 +1,21 @@
+var slideshowContainers = document.getElementsByClassName("ssc");
+for(let s = 0; s < slideshowContainers.length; s++) {
+    var cycle = slideshowContainers[s].dataset.cycle;
+    var slides = slideshowContainers[s].querySelectorAll('.ss');
+    var slideIndex = 0;
+    showSlides(slides, slideIndex, cycle);
+};
+
+function showSlides(slides, slideIndex, cycle) {
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    };
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    };
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(function() {
+        showSlides(slides, slideIndex, cycle)
+    }, cycle);
+};
